d9b8374162a6687ff9f5cef7ca66e6c191409273
VAO61 <vao.frontend@gmail.com>
2019-08-28 18:26:13 +0300

    0.0.3.0 _router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,19 +10,30 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NoResultFoundComponent } from './components/no-result-found/no-result-found.component';
+import { MyListPageComponent } from './pages/my-list-page/my-list-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
+const routes = [
+  { path: '', component: SearchPageComponent },
+  { path: 'search', component: SearchPageComponent },
+  { path: 'my-list', component: MyListPageComponent }
+  // {path: '', component: ''},
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NoResultFoundComponent
+    NoResultFoundComponent,
+    MyListPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AngularSvgIconModule
+    AngularSvgIconModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/my-list-page/my-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-list-page',
+  templateUrl: './my-list-page.component.html',
+  styleUrls: ['./my-list-page.component.scss']
+})
+export class MyListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
