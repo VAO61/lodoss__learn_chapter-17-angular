2d7e6ff4beeed457ce8928841108b689321e1fb5
VAO61 <vao.frontend@gmail.com>
2019-08-28 18:00:36 +0300

    0.0.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NoResultFoundComponent } from './no-result-found/no-result-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NoResultFoundComponent } from './components/no-result-found/no-result-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NoResultFoundComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NoResultFoundComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
