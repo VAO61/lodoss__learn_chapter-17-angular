720f117e4b3b47318952a65e39194b99eda080dc
VAO61 <vao.frontend@gmail.com>
2019-09-03 12:27:03 +0300

    0.0.5.0

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,31 +5,62 @@ import axios from 'axios';
 
 // @Injectable()
 export class DataService {
-  items = [
-    {
-      value: 1,
-      label: 'test1'
-    },
-    {
-      value: 2,
-      label: 'test2'
-    }
-  ];
-  // language: '';
-  // languages = [{ name: 'aaa' }, { name: 'bbb' }, { name: 'ccc' }];
+  type = '';
+  language = '';
+  inputValue = '';
 
-  // constructor() {}
-  // async findRepos(language) {
-  //   const response = await axios.get(
-  //     `https://api.github.com/search/repositories?q=aaa+language:${language}&sort=stars&order=desc`,
-  //     {
-  //       headers: { Accept: 'application/vnd.github.mercy-preview+json' }
-  //     }
-  //   );
-  //   console.log(response.data.items);
-  //   // console.log(this.language);
-  //   return response;
+  types = [{ value: 'Repositories' }];
+  languages = [
+    { value: 'Javascript' },
+    { value: 'CSS' },
+    { value: 'HTML' },
+    { value: 'PHP' },
+    { value: 'Ruby' },
+    { value: 'C++' },
+    { value: 'Python' },
+    { value: 'C#' },
+    { value: 'Java' },
+    { value: 'Go' },
+    { value: 'Haskel' }
+  ];
+
+  // const isDisableSearch = type === '' || lang === '' || searchValue === '';
+
+  // findRepo() {
+  //   console.log('object');
   // }
+  // constructor() {}
+  async findRepos() {
+    if (!this.type || !this.inputValue || !this.language) {
+      throw new Error(`
+        Incorrect request!
+        One or more fields is not selected.
+      `);
+    }
+    const response = await axios.get(
+      `https://api.github.com/search/${this.type}?q=${this.inputValue}+language:${this.language}&sort=stars&order=desc`,
+      {
+        headers: { Accept: 'application/vnd.github.mercy-preview+json' }
+      }
+    );
+    console.log(response.data.items);
+    console.log(`${this.type}, ${this.inputValue}, ${this.language}`);
+    return response;
+  }
+
+  typeValue(type) {
+    this.type = type.toLowerCase();
+  }
+
+  languageValue(language) {
+    this.language = language;
+  }
+
+  inputChanged(event) {
+    this.inputValue = event.target.value;
+    // console.log('aaaaa');
+  }
+
   // findRepos(language);
   // async findRepos(type, searchValue, lang) {
   //   const response = await axios.get(
