9ff25dc4f19db312969acbde72a6e4f626567507
VAO61 <vao.frontend@gmail.com>
2019-09-05 13:07:41 +0300

    0.0.7.1

--- src/app/services/get-data.service.ts
+++ src/app/services/get-data.service.ts
@@ -1,10 +1,12 @@
-// import { DataService } from './data.service';
-// import { Injectable } from '@angular/core';
+import { DataService } from './data.service';
+import { Injectable } from '@angular/core';
 import axios from 'axios';
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
-// @Injectable()
+@Injectable()
 export class GetDataService {
+  constructor(private dataService: DataService) {}
+
   type = '';
   language = '';
   inputValue = '';
@@ -24,27 +26,42 @@ export class GetDataService {
     { value: 'Haskel' }
   ];
 
-  // findRepo() {
-  //   console.log('object');
-  // }
-  // constructor() {}
-  async findRepos() {
+  findRepos$ = Observable.create(observer => {
     if (!this.type || !this.inputValue || !this.language) {
       throw new Error(`
         Incorrect request!
         One or more fields is not selected.
         `);
     }
-    const response = await axios.get(
-      `https://api.github.com/search/${this.type}?q=${this.inputValue}+language:${this.language}&sort=stars&order=desc`,
-      {
-        headers: { Accept: 'application/vnd.github.mercy-preview+json' }
-      }
-    );
-    console.log(response.data.items);
-    console.log(`${this.type}, ${this.inputValue}, ${this.language}`);
-    return response;
-  }
+
+    axios
+      .get(
+        `https://api.github.com/search/${this.type}?q=${this.inputValue}+language:${this.language}&sort=stars&order=desc`,
+        {
+          headers: { Accept: 'application/vnd.github.mercy-preview+json' }
+        }
+      )
+      .then(response => response.data.items)
+      .then(data => observer.next(data));
+  });
+
+  // async findRepos() {
+  //   if (!this.type || !this.inputValue || !this.language) {
+  //     throw new Error(`
+  //       Incorrect request!
+  //       One or more fields is not selected.
+  //       `);
+  //   }
+  //   const response = await axios.get(
+  //     `https://api.github.com/search/${this.type}?q=${this.inputValue}+language:${this.language}&sort=stars&order=desc`,
+  //     {
+  //       headers: { Accept: 'application/vnd.github.mercy-preview+json' }
+  //     }
+  //   );
+  //   console.log(response.data.items);
+  //   console.log(`${this.type}, ${this.inputValue}, ${this.language}`);
+  //   return response;
+  // }
 
   typeValue(type) {
     this.type = type.toLowerCase();
@@ -61,7 +78,15 @@ export class GetDataService {
     this.inputValue = event.target.value;
 
     if (event.which === 13 && !this.isDisableSearch()) {
-      this.findRepos();
+      this.findRepos$();
     }
+  }
+  pullRepos() {
+    this.findRepos$.subscribe(data => {
+      this.dataService.searchList = data.map(item => {
+        return item;
+      });
+      console.log(this.dataService.searchList);
+    });
   }
 }
